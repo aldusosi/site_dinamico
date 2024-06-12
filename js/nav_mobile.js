895b516bfe0ef0c248782d6e8063e312ad3c7262
aldusosi <desenho2510@gmail.com>
2024-06-12 17:30:59 -0300

    trocando icones do nav-mobile quando abre e fecha

--- js/nav_mobile.js
+++ js/nav_mobile.js
@@ -2,13 +2,18 @@ $(function(){
     $('nav.mobile').click(function(){
         
         var lista_menu = $('nav.mobile ul');
+        var icone = $('.botao-menu-mobile').find('i');
 
         if(lista_menu.is(':hidden'))
         {
+            icone.removeClass('fa-bars');
+            icone.addClass('fa-times');
             lista_menu.show();
         }
         else
         {
+            icone.removeClass('fa-times');
+            icone.addClass('fa-bars');
             lista_menu.hide();
         }
     })
